1f2fec5317f5c71e410e9a168ada2a6b73a19b7b
zl <zl.@xx.com>
2021-12-10 15:41:51 +0800

    feat: 多页面应用完成

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { injectHtml } from 'vite-plugin-html' // 插入数据到 index.html
+import del from 'rollup-plugin-delete' // 删除文件和文件夹
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig(({ command }) => { // https://cn.vitejs.dev/config/#conditional-config
+  const moduleName = process.env.MODULE_NAME
+  console.log(command);
+  return {
+    base: './',
+    plugins: [
+      vue(),
+      injectHtml({
+        data: { moduleName },
+      }),
+      (command === 'build' && del({ targets: `dist/${moduleName}` })),
+    ],
+    build: {
+      emptyOutDir: false,
+      // https://cn.vitejs.dev/config/#conditional-config
+      // https://rollupjs.org/guide/en/#outputoptions-object
+      rollupOptions: {
+        input: {
+          index: 'index.html', // 可以分为多个 .html
+        },
+        output: {
+          dir: `dist/${moduleName}`,
+        },
+      },
+    },
+  }
 })
