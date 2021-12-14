14b1dec9f8285ff81dda58e1501d1c1a873393b8
zl <zl.@xx.com>
2021-12-14 09:58:57 +0800

    docs: 更新文档

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import del from 'rollup-plugin-delete' // 删除文件和文件夹
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => { // https://cn.vitejs.dev/config/#conditional-config
   const projectName = process.env.PROJECT_NAME
-  console.log(command);
   return {
     base: './',
     plugins: [
