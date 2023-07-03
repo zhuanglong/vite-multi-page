4f82ce419253a3b7146a8d88a36f0dfb71f37cd6
zl <zl@gmail.com>
2023-07-03 16:56:24 +0800

    fix: VITE_APP_NAME

--- types/global.d.ts
+++ types/global.d.ts
@@ -40,7 +40,7 @@ declare const __APP_INFO__: AppItemInfo;
 interface ImportMetaEnv {
   // .env.xx 全局环境变量
   VITE_APP_ENV: 'dev' | 'prod' | 'staging';
-  VITE_APP_AFFILIATION_NAME: string;
+  VITE_APP_NAME: string;
 }
 interface ImportMeta {
   readonly env: ImportMetaEnv;
